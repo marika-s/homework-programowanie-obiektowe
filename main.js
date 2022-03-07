86f32f1a0655d02da455573c6dd678affa8aad7b
Marika <marikasroka@gmail.com>
2022-03-07 21:52:10 +0100

    Correction in classes, added getElementById, Fetch & event listener

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-// klasa do tworzenia obiektów, przechowujących stan aplikacji 
 class AplicationState {
     constructor() {
-        this.currentUserID = 1;
+        this.userId = 1;
         this.listOfUsers = [];
     }
-    setData(); 
-    addUser(); 
-    clearData();
+    setData(newListOfUsers) {
+        if (!newListOfUsers.length) throw Error("Array cannot be empty!");
+        this.listOfUsers = [...newListOfUsers];
+    }
+    addUser(user) {
+        if (!user.id) throw Error("User property is empty!");
+        this.userId += 1;
+        this.listOfUsers.push(user);
+    }
+    clearData() {
+        this.userId = 1;
+        this.listOfUsers = [];
+    }
 }
 
-// obecny stan aplikacji stworzony na podstawie klasy AplicationState
 const currentStateOfApplication = new AplicationState();
 
-// klasa do tworzenia obiektów z danymi pobranego usera
 class User {
     constructor(id, name, username, email, address) {
         this.id = id;
@@ -21,5 +28,43 @@ class User {
         this.email = email;
         this.address = address;
     }
-
+    get fullName() {
+        return `${this.name} ${this.username}`;
+    }
+    set updatedFullName(updatedFullName){
+        [this.name, this.username] = updatedFullName.split(" ");
+    }
+    get FullAddress() {
+        return `${this.address.street} ${this.address.suite}, ${this.address.zipcode} ${this.address.city}`;
+    }
 };
+
+const getUserDataButton = document.getElementById("getUserDataButton");
+const userList = document.getElementById("usersList");
+const currentUserId = document.getElementById("currentUserId");
+
+
+
+const onGetUserDataButtonClick = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${currentStateOfApplication.userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const { id, name, username, email, address} = data;
+        const user = new User(id, name, username, email, address);
+       
+        currentStateOfApplication.addUser(user); // OK
+          
+        updateUserIdToHtml(id); //to do
+        addUserToHtml(user, userList); //to do
+      })
+      .catch((error) => console.log(`${error}`));
+};
+
+  getUserDataButton.addEventListener("click", onGetUserDataButtonClick);
+
+  function updateUserIdToHtml() {}; //to do
+
+  function addUserToHtml() {}; //to do
+
+
+
