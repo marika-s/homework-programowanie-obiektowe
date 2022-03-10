11a17d92e0f394b044b4f5f34b7a37e5ca2d2263
Marika <marikasroka@gmail.com>
2022-03-10 22:41:04 +0100

    deleting users from application state

--- main.js
+++ main.js
@@ -1,95 +1,93 @@
 class AplicationState {
-    constructor() {
-        this.userId = 1;
-        this.listOfUsers = [];
-    }
-    setData(newListOfUsers) {
-        if (!newListOfUsers.length) throw Error("Array cannot be empty!");
-        this.listOfUsers = [...newListOfUsers];
-    }
-    addUser(user) {
-        if (!user.id) throw Error("User property is empty!");
-        this.userId += 1;
-        this.listOfUsers.push(user);
-    }
-    clearData() {
-        this.userId = 1;
-        this.listOfUsers = [];
-    }
+  constructor() {
+    this.userId = 1;
+    this.listOfUsers = [];
+  }
+  setData(newListOfUsers) {
+    if (!newListOfUsers.length) throw Error("Array cannot be empty!");
+    this.listOfUsers = [...newListOfUsers];
+  }
+  addUser(user) {
+    if (!user.id) throw Error("User property is empty!");
+    this.userId += 1;
+    this.listOfUsers.push(user);
+  }
+  clearData() {
+    this.userId = 1;
+    this.listOfUsers = [];
+  }
 }
 
 const currentStateOfApplication = new AplicationState();
 
 class User {
-    constructor(id, name, username, email, address) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.address = address;
-    }
-    get fullName() {
-        return `${this.name} ${this.username}`;
-    }
-    set updatedFullName(updatedFullName){
-        [this.name, this.username] = updatedFullName.split(" ");
-    }
-    get FullAddress() {
-        return `${this.address.street} ${this.address.suite}, ${this.address.zipcode} ${this.address.city}`;
-    }
-};
+  constructor(id, name, username, email, address) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.address = address;
+  }
+  get fullName() {
+    return `${this.name} ${this.username}`;
+  }
+  set updatedFullName(updatedFullName) {
+    [this.name, this.username] = updatedFullName.split(" ");
+  }
+  get FullAddress() {
+    return `${this.address.street} ${this.address.suite}, ${this.address.zipcode} ${this.address.city}`;
+  }
+}
 
 const getUserDataButton = document.getElementById("getUserDataButton");
 const userList = document.getElementById("usersList");
 const currentUserId = document.getElementById("currentUserId");
 
 const onGetUserDataButtonClick = () => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${currentStateOfApplication.userId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const { id, name, username, email, address} = data;
-        const user = new User(id, name, username, email, address);
-       
-        currentStateOfApplication.addUser(user);
-          
-        updateUserIdToHtml(id); 
-        addUserToHtml(user);
-      })
-      .catch((error) => console.log(`${error}`));
+  fetch(
+    `https://jsonplaceholder.typicode.com/users/${currentStateOfApplication.userId}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const { id, name, username, email, address } = data;
+      const user = new User(id, name, username, email, address);
+
+      currentStateOfApplication.addUser(user);
+
+      updateUserIdToHtml(id);
+      addUserToHtml(user);
+    })
+    .catch((error) => console.log(`${error}`));
 };
 
-  getUserDataButton.addEventListener("click", onGetUserDataButtonClick);
+getUserDataButton.addEventListener("click", onGetUserDataButtonClick);
 
-  function updateUserIdToHtml(id) {
-      currentUserId.innerHTML = id; 
-  }; 
+function updateUserIdToHtml(id) {
+  currentUserId.innerHTML = id;
+}
 
-  function addUserToHtml(user) {
-      const userHtmlElement = document.createElement("li");
-      userList.appendChild(userHtmlElement);
-      userHtmlElement.innerHTML = `<p id="user-id${user.id}">#${user.id}: ${user.name} ${user.username} </p>`;
-  }; 
+function addUserToHtml(user) {
+  const userHtmlElement = document.createElement("li");
+  userList.appendChild(userHtmlElement);
+  userHtmlElement.innerHTML = `<p id="user-id${user.id}">#${user.id}: ${user.name} ${user.username} </p>`;
+}
 
 // Homework
 
-function removeClickedUser(event) {    
-    const clickedUser = event.target.id; 
-    const clickedUserId = document.getElementById(`${clickedUser}`);
-    clickedUserId.parentNode.remove();
-    
-    
+function removeClickedUser(event) {
+  const clickedUser = event.target.id;
+  const clickedUserTag = document.getElementById(clickedUser);
+  clickedUserTag.parentNode.remove();
 
+  const userID = clickedUser.match(/\d/g).join(""); // jak tu zrobić destrukturyzacją? w clickedUser dostaję identyfikator, np. user-id1 i chciałabym się dobrać do 1. Tu poległam.
+  const userIdAsNumber = userID * 1;
+  removeUserFromApplicationState();
 
-    //deleteUserfromAplicationState();
-   
-};
-
+  function removeUserFromApplicationState() {
+    let indexToRemove = currentStateOfApplication.listOfUsers.findIndex(
+      (data) => data.id === userIdAsNumber
+    );
+    currentStateOfApplication.listOfUsers.splice(indexToRemove, 1);
+  }
+}
 userList.addEventListener("click", removeClickedUser);
-
-
-
-
-
-
-
-
